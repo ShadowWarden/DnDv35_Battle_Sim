e98f2c8b21dc7973acdf2cd131644c584595dfc2
Omkar Ramachandran <omkar.ramachandran@gmail.com>
2017-07-26 22:51:35 -0600

    Renamed Char to Fighter

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 import sys
 import random as rand
 
-class Char:
+class Fighter:
     def __init__(self,st,iq,wz,co,ch,dx,attack,damage,AC,hpts):
         self.st = st
         self.iq = iq
@@ -20,6 +20,7 @@ class Char:
         self.damage = damage
         self.AC = AC
         self.hpts = hpts;
+
     def initiative(self):
         return rand.randint(1,20)+self.dx
     def full_ac(self):
@@ -31,7 +32,6 @@ class Char:
     def take_damage(self,dam):
         self.hpts -= dam
 
-
 def main():
     N_fights = 1000
     f1_wins = 0
@@ -39,8 +39,9 @@ def main():
     i = 0
 
     while(i < N_fights):
-        fighter1 = Char(15,10,11,16,11,14,6,8,15,30)
-        fighter2 = Char(15,10,11,16,11,14,6,8,15,30)
+        fighter1 = Fighter(15,10,11,16,11,14,6,8,15,39)
+        fighter2 = Fighter(15,10,11,16,11,14,6,8,17,30)
+        
         init1 = fighter1.initiative()
         init2 = fighter2.initiative()
         
@@ -54,18 +55,12 @@ def main():
                 if(to_hit > fighter1.full_ac()):
                     damage = fighter2.wpn_damage()
                     fighter1.take_damage(damage)
-                #    print("Fighter 2 hits dealing %d damage! Fighter 1 has %d hitpoints left") % (damage, fighter1.hpts)
-                #else:
-                #    print("Fighter 2 swings and misses")
                 flag = 0
             else:
                 to_hit = fighter1.to_hit()
                 if(to_hit > fighter2.full_ac()):
                     damage = fighter1.wpn_damage()
                     fighter2.take_damage(damage)
-#                print ("Fighter 1 hits dealing %d damage! Fighter 2 has %d hitpoints left") % (fighter1.wpn_damage(), fighter2.hpts)
-#            else:
-#                print ("Fighter 1 swings and misses")
                 flag = 1
         if(fighter1.hpts>0):
             f1_wins += 1
